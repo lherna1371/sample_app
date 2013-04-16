485c067d08e04dd6c6f160eb78a0981b9bf9eca1
Luis Hernandez <lherna1371@gmail.com>
2013-04-16 12:04:13 -0500

    Add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,120 +1,171 @@
 require 'spec_helper'
 
-describe User do
+describe "User pages" do
 
-  before do
-    @user = User.new(name: "Example User", email: "user@example.com", 
-                     password: "foobar", password_confirmation: "foobar")
+  subject { page }
+
+  describe "index" do
+    
+    before do
+      sign_in FactoryGirl.create(:user)
+      FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
+      FactoryGirl.create(:user, name: "Ben", email: "ben@example.com")
+      visit users_path
+    end
+
+    it { should have_selector('title', text: 'All users') }
+
+    describe "pagination" do
+      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      after(:all)  { User.delete_all }
+
+      let(:first_page)  { User.paginate(page: 1) }
+      let(:second_page) { User.paginate(page: 2) }
+
+      it { should have_link('Next') }
+      it { should have_link('2') }
+
+      it "should list each user" do
+        User.all[0..2].each do |user|
+          page.should have_selector('li', text: user.name)
+        end
+      end
+
+      it "should list the first page of users" do
+        first_page.each do |user|
+          page.should have_selector('li', text: user.name)
+        end
+      end
+
+      it "should not list the second page of users" do
+        second_page.each do |user|
+          page.should_not have_selector('li', text: user.name)
+        end
+      end
+      
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect { click_link('delete') }.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
+      end   
+    end
   end
 
-  subject { @user }
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
 
-  it { should respond_to(:name) }
-  it { should respond_to(:email) }
-  it { should respond_to(:password_digest) }
-  it { should respond_to(:password) }
-  it { should respond_to(:password_confirmation) }
-  it { should respond_to(:remember_token) }
-  it { should respond_to(:admin) }
-  it { should respond_to(:authenticate) }
+    before { visit user_path(user) }
 
-  it { should be_valid }
-  it { should_not be_admin }
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
 
-  describe "with admin attribute set to 'true'" do
-    before { @user.toggle!(:admin) }
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
 
-    it { should be_admin }
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
+  end
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_selector('h1',    text: 'Sign up') }
+    it { should have_selector('title', text: full_title('Sign up')) }
+  end
+
+  describe "signup" do
+
+    before { visit signup_path }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button "Create my account" }.not_to change(User, :count)
+      end
+      
+      describe "error messages" do
+        before { click_button "Create my account" }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect do
+          click_button "Create my account"
+        end.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button "Create my account" }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+      end
+    end
   end
   
-  describe "when name is not present" do
-    before { @user.name = " " }
-    it { should_not be_valid }
-  end
-
-  describe "when email is not present" do
-    before { @user.email = " " }
-    it { should_not be_valid }
-  end
-
-  describe "when name is too long" do
-    before { @user.name = "a" * 51 }
-    it { should_not be_valid }
-  end
-
-  describe "when email format is invalid" do
-    invalid_addresses =  %w[user@foo,com user_at_foo.org example.user@foo.]
-    invalid_addresses.each do |invalid_address|
-      before { @user.email = invalid_address }
-      it { should_not be_valid }
-    end
-  end
-
-  describe "when email format is valid" do
-    valid_addresses = %w[user@foo.com A_USER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-    valid_addresses.each do |valid_address|
-      before { @user.email = valid_address }
-      it { should be_valid }
-    end
-  end
-
-  describe "when email address is already taken" do
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
     before do
-      user_with_same_email = @user.dup
-      user_with_same_email.save
+      sign_in user
+      visit edit_user_path(user)
     end
 
-    it { should_not be_valid }
-  end
-
-  describe "when email address is already taken" do
-    before do
-      user_with_same_email = @user.dup
-      user_with_same_email.email = @user.email.upcase
-      user_with_same_email.save
+    describe "page" do
+      it { should have_selector('h1',    text: "Update your profile") }
+      it { should have_selector('title', text: "Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
     end
 
-    it { should_not be_valid }
-  end
+    describe "with invalid information" do
+      before { click_button "Save changes" }
 
-  describe "when password is not present" do
-    before { @user.password = @user.password_confirmation = " " }
-    it { should_not be_valid }
-  end
-
-  describe "when password doesn't match confirmation" do
-    before { @user.password_confirmation = "mismatch" }
-    it { should_not be_valid }
-  end
-
-  describe "when password confirmation is nil" do
-    before { @user.password_confirmation = nil }
-    it { should_not be_valid }
-  end
-
-  describe "with a password that's too short" do
-    before { @user.password = @user.password_confirmation = "a" * 5 }
-    it { should be_invalid }
-  end
-
-  describe "return value of authenticate method" do
-    before { @user.save }
-    let(:found_user) { User.find_by_email(@user.email) }
-
-    describe "with valid password" do
-      it { should == found_user.authenticate(@user.password) }
+      it { should have_content('error') }
     end
 
-    describe "with invalid password" do
-      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+    describe "with valid information" do
+      let(:user)      { FactoryGirl.create(:user) }
+      let(:new_name)  { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Name",             with: new_name
+        fill_in "Email",            with: new_email
+        fill_in "Password",         with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
+      end
 
-      it { should_not == user_for_invalid_password }
-      specify { user_for_invalid_password.should be_false }
+      it { should have_selector('title', text: new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out', :href => signout_path) }
+      specify { user.reload.name.should  == new_name }
+      specify { user.reload.email.should == new_email }
     end
-  end
-
-  describe "remember token" do
-    before { @user.save }
-    its(:remember_token) { should_not be_blank }
   end
 end
